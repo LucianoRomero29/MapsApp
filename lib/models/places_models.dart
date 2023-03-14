c7e2b1d732ec31d076bdf75ab06628522da1cbfe
LucianoRomero1 <Lucianooromero1@gmail.com>
2023-03-14 15:31:45 -0300

    Fix model places

--- lib/models/places_models.dart
+++ lib/models/places_models.dart
@@ -1,37 +1,37 @@
 // To parse this JSON data, do
 //
-//     final placesResponse = placesResponseFromMap(jsonString);
+//     final placesResponse = placesResponseFromJson(jsonString);
 
 import 'dart:convert';
 
 class PlacesResponse {
     PlacesResponse({
         required this.type,
-        // required this.query,
+        required this.query,
         required this.features,
         required this.attribution,
     });
 
     final String type;
-    // final List<String> query;
+    final List<double> query;
     final List<Feature> features;
     final String attribution;
 
-    factory PlacesResponse.fromJson(String str) => PlacesResponse.fromMap(json.decode(str));
+    factory PlacesResponse.fromRawJson(String str) => PlacesResponse.fromJson(json.decode(str));
 
-    String toJson() => json.encode(toMap());
+    String toRawJson() => json.encode(toJson());
 
-    factory PlacesResponse.fromMap(Map<String, dynamic> json) => PlacesResponse(
+    factory PlacesResponse.fromJson(Map<String, dynamic> json) => PlacesResponse(
         type: json["type"],
-        // query: List<String>.from(json["query"].map((x) => x)),
-        features: List<Feature>.from(json["features"].map((x) => Feature.fromMap(x))),
+        query: List<double>.from(json["query"].map((x) => x?.toDouble())),
+        features: List<Feature>.from(json["features"].map((x) => Feature.fromJson(x))),
         attribution: json["attribution"],
     );
 
-    Map<String, dynamic> toMap() => {
+    Map<String, dynamic> toJson() => {
         "type": type,
-        // "query": List<dynamic>.from(query.map((x) => x)),
-        "features": List<dynamic>.from(features.map((x) => x.toMap())),
+        "query": List<dynamic>.from(query.map((x) => x)),
+        "features": List<dynamic>.from(features.map((x) => x.toJson())),
         "attribution": attribution,
     };
 }
@@ -41,15 +41,10 @@ class Feature {
         required this.id,
         required this.type,
         required this.placeType,
+        required this.relevance,
         required this.properties,
-        required this.textEs,
-        // required this.languageEs,
-        required this.placeNameEs,
         required this.text,
-        required this.language,
         required this.placeName,
-        required this.matchingText,
-        required this.matchingPlaceName,
         required this.center,
         required this.geometry,
         required this.context,
@@ -58,182 +53,121 @@ class Feature {
     final String id;
     final String type;
     final List<String> placeType;
+    final int relevance;
     final Properties properties;
-    final String textEs;
-    // final Language? languageEs;
-    final String placeNameEs;
     final String text;
-    final String? language;
     final String placeName;
-    final String? matchingText;
-    final String? matchingPlaceName;
     final List<double> center;
     final Geometry geometry;
     final List<Context> context;
 
-    factory Feature.fromJson(String str) => Feature.fromMap(json.decode(str));
+    factory Feature.fromRawJson(String str) => Feature.fromJson(json.decode(str));
 
-    String toJson() => json.encode(toMap());
+    String toRawJson() => json.encode(toJson());
 
-    factory Feature.fromMap(Map<String, dynamic> json) => Feature(
+    factory Feature.fromJson(Map<String, dynamic> json) => Feature(
         id: json["id"],
         type: json["type"],
         placeType: List<String>.from(json["place_type"].map((x) => x)),
-        properties: Properties.fromMap(json["properties"]),
-        textEs: json["text_es"],
-        // languageEs: json["language_es"] == null ? null : languageValues.map[json["language_es"]],
-        placeNameEs: json["place_name_es"],
+        relevance: json["relevance"],
+        properties: Properties.fromJson(json["properties"]),
         text: json["text"],
-        language: json["language"],
         placeName: json["place_name"],
-        matchingText: json["matching_text"],
-        matchingPlaceName: json["matching_place_name"],
-        center: List<double>.from(json["center"].map((x) => x.toDouble())),
-        geometry: Geometry.fromMap(json["geometry"]),
-        context: List<Context>.from(json["context"].map((x) => Context.fromMap(x))),
+        center: List<double>.from(json["center"].map((x) => x?.toDouble())),
+        geometry: Geometry.fromJson(json["geometry"]),
+        context: List<Context>.from(json["context"].map((x) => Context.fromJson(x))),
     );
 
-    Map<String, dynamic> toMap() => {
+    Map<String, dynamic> toJson() => {
         "id": id,
         "type": type,
         "place_type": List<dynamic>.from(placeType.map((x) => x)),
-        "properties": properties.toMap(),
-        "text_es": textEs,
-        // "language_es": languageEs == null ? null : languageValues.reverse[languageEs],
-        "place_name_es": placeNameEs,
+        "relevance": relevance,
+        "properties": properties.toJson(),
         "text": text,
-        "language": language,
         "place_name": placeName,
-        "matching_text": matchingText,
-        "matching_place_name": matchingPlaceName,
         "center": List<dynamic>.from(center.map((x) => x)),
-        "geometry": geometry.toMap(),
-        "context": List<dynamic>.from(context.map((x) => x.toMap())),
+        "geometry": geometry.toJson(),
+        "context": List<dynamic>.from(context.map((x) => x.toJson())),
     };
-
-    @override
-  String toString() {
-    return 'Feature: $text';
-  }
 }
 
 class Context {
     Context({
         required this.id,
-        required this.textEs,
+        required this.mapboxId,
         required this.text,
-        required this.wikidata,
-        // required this.languageEs,
-        required this.language,
-        required this.shortCode,
+        this.wikidata,
+        this.shortCode,
     });
 
     final String id;
-    final String textEs;
+    final String mapboxId;
     final String text;
     final String? wikidata;
-    // final Language? languageEs;
-    final String? language;
     final String? shortCode;
 
-    factory Context.fromJson(String str) => Context.fromMap(json.decode(str));
+    factory Context.fromRawJson(String str) => Context.fromJson(json.decode(str));
 
-    String toJson() => json.encode(toMap());
+    String toRawJson() => json.encode(toJson());
 
-    factory Context.fromMap(Map<String, dynamic> json) => Context(
+    factory Context.fromJson(Map<String, dynamic> json) => Context(
         id: json["id"],
-        textEs: json["text_es"],
+        mapboxId: json["mapbox_id"],
         text: json["text"],
         wikidata: json["wikidata"],
-        // languageEs: json["language_es"],
-        language: json["language"],
         shortCode: json["short_code"],
     );
 
-    Map<String, dynamic> toMap() => {
+    Map<String, dynamic> toJson() => {
         "id": id,
-        "text_es": textEs,
+        "mapbox_id": mapboxId,
         "text": text,
         "wikidata": wikidata,
-        // "language_es": languageEs == null ? null : languageValues.reverse[languageEs],
-        "language": language,
         "short_code": shortCode,
     };
 }
 
-
 class Geometry {
     Geometry({
-        required this.coordinates,
         required this.type,
+        required this.coordinates,
     });
 
-    final List<double> coordinates;
     final String type;
+    final List<double> coordinates;
 
-    factory Geometry.fromJson(String str) => Geometry.fromMap(json.decode(str));
+    factory Geometry.fromRawJson(String str) => Geometry.fromJson(json.decode(str));
 
-    String toJson() => json.encode(toMap());
+    String toRawJson() => json.encode(toJson());
 
-    factory Geometry.fromMap(Map<String, dynamic> json) => Geometry(
-        coordinates: List<double>.from(json["coordinates"].map((x) => x.toDouble())),
+    factory Geometry.fromJson(Map<String, dynamic> json) => Geometry(
         type: json["type"],
+        coordinates: List<double>.from(json["coordinates"].map((x) => x?.toDouble())),
     );
 
-    Map<String, dynamic> toMap() => {
-        "coordinates": List<dynamic>.from(coordinates.map((x) => x)),
+    Map<String, dynamic> toJson() => {
         "type": type,
+        "coordinates": List<dynamic>.from(coordinates.map((x) => x)),
     };
 }
 
 class Properties {
     Properties({
-        required this.wikidata,
-        required this.category,
-        required this.landmark,
-        required this.address,
-        required this.foursquare,
-        required this.maki,
+        required this.accuracy,
     });
 
-    final String? wikidata;
-    final String? category;
-    final bool? landmark;
-    final String? address;
-    final String? foursquare;
-    final String? maki;
+    final String accuracy;
 
-    factory Properties.fromJson(String str) => Properties.fromMap(json.decode(str));
+    factory Properties.fromRawJson(String str) => Properties.fromJson(json.decode(str));
 
-    String toJson() => json.encode(toMap());
+    String toRawJson() => json.encode(toJson());
 
-    factory Properties.fromMap(Map<String, dynamic> json) => Properties(
-        wikidata: json["wikidata"],
-        category: json["category"],
-        landmark: json["landmark"],
-        address: json["address"],
-        foursquare: json["foursquare"],
-        maki: json["maki"],
+    factory Properties.fromJson(Map<String, dynamic> json) => Properties(
+        accuracy: json["accuracy"],
     );
 
-    Map<String, dynamic> toMap() => {
-        "wikidata": wikidata,
-        "category": category,
-        "landmark": landmark,
-        "address": address,
-        "foursquare": foursquare,
-        "maki": maki,
+    Map<String, dynamic> toJson() => {
+        "accuracy": accuracy,
     };
-}
-
-class EnumValues<T> {
-    Map<String, T> map;
-    Map<T, String>? reverseMap;
-
-    EnumValues(this.map);
-
-    Map<T, String> get reverse {
-        reverseMap ??= map.map((k, v) => MapEntry(v, k));
-        return reverseMap!;
-    }
 }
